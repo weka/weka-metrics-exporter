d125ee9ec0a0a7e1e503270d9e7ab89f305b4281
Vince Fleming <vince@weka.io>
2020-10-05 14:31:29 -0400

    changed to allow list of weka servers

--- start_exporter.sh
+++ start_exporter.sh
@@ -6,9 +6,18 @@ if [ $# -eq 0 ]
     exit
 fi
 
+# Handle case where we have a comma-separated list of weka servers
+IFS=',' read -r -a ADDRESSES <<< "$@"
 
-ADDRESS=$@
+for ADDRESS in "${ADDRESSES[@]}"
+do
+    # try to install the weka agent (weka command) from the server(s).  First one to install wins
+    timeout 10.0 curl -s http://$ADDRESS:14000/dist/v1/install | sh &> /dev/null
+    if [ $? == 0 ]; then
+        # success! move on
+        break
+    fi
+done
 
-curl -s http://$ADDRESS:14000/dist/v1/install | timeout 10 sh 1> /dev/null 2>/dev/null
 cd /root/ && ./weka-metrics-exporter.py -a -H $@
 
